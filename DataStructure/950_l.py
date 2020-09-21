4cb4dba9cdf4b6f2f05203c9efe91ac8f3ab2599
khj68 <maga40@naver.com>
2020-09-21 22:10:09 +0900

    Solve leetcode 950

---
+++ DataStructure/950_l.py
@@ -0,0 +1,8 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        d = collections.deque()
+        for x in sorted(deck)[::-1]:
+            d.rotate()
+            d.appendleft(x)
+            # print(d)
+        return list(d)
