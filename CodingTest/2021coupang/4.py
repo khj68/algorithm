f347fc7ec999fccf416415fc84eacd346f2b066e
KHJ <maga40@naver.com>
2020-10-20 15:04:16 +0900

    Solve problem

--- CodingTest/2021coupang/4.py
+++ CodingTest/2021coupang/4.py
@@ -34,4 +34,3 @@ def solution(depar, hub, dest, roads):
 print(solution("SEOUL", "DAEGU", "YEOSU", [["ULSAN","BUSAN"],["DAEJEON","ULSAN"],["DAEJEON","GWANGJU"],["SEOUL","DAEJEON"],["SEOUL","ULSAN"],["DAEJEON","DAEGU"],["GWANGJU","BUSAN"],["DAEGU","GWANGJU"],["DAEGU","BUSAN"],["ULSAN","DAEGU"],["GWANGJU","YEOSU"],["BUSAN","YEOSU"]]))
 print(solution("ULSAN", "SEOUL", "BUSAN", [["SEOUL","DAEJEON"],["ULSAN","BUSAN"],["DAEJEON","ULSAN"],["DAEJEON","GWANGJU"],["SEOUL","ULSAN"],["DAEJEON","BUSAN"],["GWANGJU","BUSAN"]]))
 # print(solution())
-# print(solution())
